407ec1d3a025f2a87295ffe83eebff3bc04f1c3e
Marcos Oliveira <marcos@terminalroot.com.br>
2025-07-01 11:50:04 -0300

    🎨 adjusts

--- hexter.hpp
+++ hexter.hpp
@@ -8,15 +8,15 @@
 #include <algorithm>
 
 class HexTer {
+
+  std::string hex_to_ansi_rgb(const std::string&, int&, int&, int&);
+  std::string ansi_color(int, int, int, bool);
+  std::string bg_color(int, int, int);
+  std::string repeat(const std::string&, size_t);
+  std::string strip_ansi(const std::string&);
+  void print_table(const std::string&, int, int, int);
+
   public:
-
-    std::string hex_to_ansi_rgb(const std::string&, int&, int&, int&);
-    std::string ansi_color(int, int, int, bool);
-    std::string bg_color(int, int, int);
-    std::string repeat(const std::string&, size_t);
-    std::string strip_ansi(const std::string&);
-    void print_table(const std::string&, int, int, int);
-
     HexTer();
     void run(int, char**);
 };
